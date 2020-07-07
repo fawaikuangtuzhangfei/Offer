e94ecef1ea8ec3ff7c46fdcab16bc26bf028396c
1 <1>
2020-07-07 15:15:27 +0800

    d48二叉搜索树的第k个结点

--- 50/d48二叉搜索树的第k个结点/Solution2.java
+++ 50/d48二叉搜索树的第k个结点/Solution2.java
@@ -1,25 +1,23 @@
 package d48二叉搜索树的第k个结点;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 /**
  * 给定一棵二叉搜索树，请找出其中的第k小的结点。例如， （5，3，7，2，4，6，8）    中，按结点数值大小顺序第三小结点的值为4。
- * 二叉搜索树 左<中<右 所以中序遍历就是递增的
- * 第k个就是第k个
+ * 
+ * 找到第k个直接return，无需list
  * @Description: TODO
  * @author nanshoudabaojian
  * @date 2020年7月3日 下午3:50:04
  */
-public class Solution {
+public class Solution2 {
     TreeNode KthNode(TreeNode pRoot, int k)
     {
     	if(k == 0 || pRoot == null){
     		return null;
     	}
-    	List<TreeNode> list = new ArrayList<>();
     	Stack<TreeNode> stack = new Stack<>();
+    	int count = 1;
     	
         while(pRoot != null || !stack.isEmpty()){
         	if(pRoot != null){
@@ -27,27 +25,15 @@ public class Solution {
         		pRoot = pRoot.left;
         	}else{
         		pRoot = stack.pop();
-        		list.add(pRoot);
+        		if(k == count++){
+        			return pRoot;
+        		}
         		pRoot = pRoot.right;
         	}
         }
-        if(k > list.size()){
-        	return null;
-        }
-        return list.get(k-1);
+        return null;
     }
 
 
 }
 
-class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
-
-    public TreeNode(int val) {
-        this.val = val;
-
-    }
-
-}
